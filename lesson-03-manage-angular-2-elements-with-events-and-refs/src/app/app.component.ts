498741b13f89d241b51e89ee4e4421211d420794
Sam_Grinis <sam.grinis@gmail.com>
2018-10-30 11:05:05 -0400

    updated to angular 7

---
+++ lesson-03-manage-angular-2-elements-with-events-and-refs/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `<div>
+
+<app-simple-form></app-simple-form>
+
+</div>`
+})
+export class AppComponent {
+  title = `Let's get started!`;
+}
---
+++ lesson-11-write-css-inside-of-angular-2-components/src/app/simple-form/simple-form.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-simple-form',
+  template: `
+<input #myInput type="text" [(ngModel)]="message">
+<button (click)="update.emit({text:message})">Click me!</button>
+`,
+  styles: [`
+:host{
+  display: flex;
+  flex-direction: column;
+}
+
+input:focus{
+  font-weight: bold;
+  outline: none;
+}
+
+button{
+  border: none;
+}
+`]
+})
+export class SimpleFormComponent implements OnInit {
+
+  @Input() message;
+
+  @Output() update = new EventEmitter();
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
